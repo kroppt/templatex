ea434bf6a7cc98dedc0c7ca74d2596f9bcd26c7f
Tyler Kropp <kropptyler@gmail.com>
2018-08-18 01:20:27 -0400

    Add LaTeX escaper functionality

--- pkg/escaper/latex.go
+++ pkg/escaper/latex.go
@@ -1,8 +1,29 @@
 package escaper
 
+import "strings"
+
 type latexEscaper struct {
 }
 
 func (esc latexEscaper) Escape(in string) (out string) {
+	out = in
+	m := make(map[string]string)
+	m["\\"] = "\\textbackslash"
+	m["&"] = "\\&"
+	m["%"] = "\\%"
+	m["$"] = "\\$"
+	m["#"] = "\\#"
+	m["_"] = "\\_"
+	m["{"] = "\\{"
+	m["}"] = "\\}"
+	m["~"] = "\\textasciitilde"
+	m["^"] = "\\textasciicircum"
+	// replace gobbled spaces
+	m["\\textbackslash "] = "\\textbackslash~"
+	m["\\textasciitilde "] = "\\textasciitilde~"
+	m["\\textasciicircum "] = "\\textasciicircum~"
+	for k, v := range m {
+		strings.Replace(out, k, v, -1)
+	}
 	return out
 }
