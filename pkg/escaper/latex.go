8d0bb35cf80afba0392c3800e40d1685f0d1abf1
Tyler Kropp <kropptyler@gmail.com>
2018-08-18 19:50:17 -0400

    Add in-order replacing to LaTeX escaper

--- pkg/escaper/latex.go
+++ pkg/escaper/latex.go
@@ -11,23 +11,26 @@ type latexEscaper struct {
 
 func (esc latexEscaper) Escape(in string) (out string) {
 	out = in
-	m := make(map[string]string)
-	m["\\"] = "\\textbackslash"
-	m["&"] = "\\&"
-	m["%"] = "\\%"
-	m["$"] = "\\$"
-	m["#"] = "\\#"
-	m["_"] = "\\_"
-	m["{"] = "\\{"
-	m["}"] = "\\}"
-	m["~"] = "\\textasciitilde"
-	m["^"] = "\\textasciicircum"
-	// replace gobbled spaces
-	m["\\textbackslash "] = "\\textbackslash~"
-	m["\\textasciitilde "] = "\\textasciitilde~"
-	m["\\textasciicircum "] = "\\textasciicircum~"
-	for k, v := range m {
-		strings.Replace(out, k, v, -1)
+	steps := []struct {
+		i, o string
+	}{
+		{"\\", "\\textbackslash"},
+		{"&", "\\&"},
+		{"%", "\\%"},
+		{"$", "\\$"},
+		{"#", "\\#"},
+		{"_", "\\_"},
+		{"{", "\\{"},
+		{"}", "\\}"},
+		{"~", "\\textasciitilde"},
+		{"^", "\\textasciicircum"},
+		// replace gobbled spaces
+		{"\\textbackslash ", "\\textbackslash~"},
+		{"\\textasciitilde ", "\\textasciitilde~"},
+		{"\\textasciicircum ", "\\textasciicircum~"},
+	}
+	for _, st := range steps {
+		strings.Replace(out, st.i, st.o, -1)
 	}
 	return out
 }
